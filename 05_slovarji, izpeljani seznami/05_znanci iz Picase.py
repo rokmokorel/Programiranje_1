5120557becf9a578d6bd4af047d2d90cba96e40e
rok <rok.mokorel@gmail.com>
2018-03-13 15:42:18 +0100

    Commit iz sistema Linux

--- 05_slovarji, izpeljani seznami/05_znanci iz Picase.py
+++ 05_slovarji, izpeljani seznami/05_znanci iz Picase.py
@@ -1,25 +1,31 @@
 datoteka = open('izvoz.xml', mode='r')
-
-
+'''
 for vrstica in datoteka:
-    drzava = vrstica.split()[0]
-    dobrine = vrstica.split()[1:]
+    drzava = vrstica.split('\t ')[0]
+    dobrine = vrstica.split('\t ')[1].split(', ')
+    dobrine[-1] = dobrine[-1].strip('\n')
     print('{}\n{}\n'.format(drzava, dobrine))
+'''
 
 drzave = {}
 produkti = {}
 for vrstica in datoteka:
-    sez_produktov = vrstica.split()[1:]
-    ime_drzave = vrstica.split()[0]
+    ime_drzave = vrstica.split('\t ')[0]
+    sez_produktov = vrstica.split('\t ')[1].split(', ')
+    sez_produktov[-1] = sez_produktov[-1].strip('\n')
+
     drzave[ime_drzave] = sez_produktov
     for ime in sez_produktov:
         if ime not in produkti:
             produkti[ime] = [ime_drzave]
         else:
-            produkti[ime] += ime_drzave
+            produkti[ime].append(ime_drzave)
 
 def kdo_izvaza(produkt):
     return produkti[produkt]
 
 def kaj_izvaza(drzava):
     return drzave[drzava]
+
+print(kaj_izvaza('Puerto Rico'))
+print(kdo_izvaza('rum'))
