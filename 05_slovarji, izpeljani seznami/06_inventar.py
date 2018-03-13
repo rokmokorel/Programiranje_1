29ed6ec19f2cc079eb4f6a17459880de9a14d185
rok <rok.mokorel@gmail.com>
2018-03-13 16:23:32 +0100

    Commit iz sistema Linux

--- 05_slovarji, izpeljani seznami/06_inventar.py
+++ 05_slovarji, izpeljani seznami/06_inventar.py
@@ -1,15 +1,17 @@
 datoteka = open('preberi_inventar.txt')
 
+
 def preberi_inventar(ime_dat):
     inventar = {}
     for vrstica in ime_dat:
         izdelek, kolicina = vrstica.split('\t')
         inventar[izdelek] = int(kolicina.strip())
     return inventar
+
 print(preberi_inventar(datoteka))
 
-inventar = preberi_inventar(datoteka)
-print(inventar)
 
+'''
 def zaloga(inventar, artikel):
     return inventar[artikel]
+'''
